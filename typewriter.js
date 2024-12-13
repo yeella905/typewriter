bfaf29eb1dbde328d3ccfc0511daf1057a03d972
yeella905 <yeella905@gmail.com>
2024-12-13 16:42:49 -0500

    1st pass, made the function haven't solved the delays after each character

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const sentence = "hello there from lighthouse labs";
+
+for (const char of sentence) {
+    setTimeout( function timer () {
+        console.log(char);
+      }, char * 1000);
+};
